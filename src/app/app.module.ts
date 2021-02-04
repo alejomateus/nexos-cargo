4c33a8044ffc58e4e7f3ad4b38c84f9ebc80e5fe
Miguel Corzo <johndoe@avaldigitallabs.com>
2021-02-03 20:29:57 -0500

    :memo: Seccion contacto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
-
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, ComponentsModule],
--- src/app/modules/contacto/contacto.module.ts
+++ src/app/modules/contacto/contacto.module.ts
@@ -3,9 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { ContactoRoutingModule } from './contacto-routing.module';
 import { ContactoComponent } from './contacto.component';
+import { AgmCoreModule } from '@agm/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ContactoComponent],
-  imports: [CommonModule, ContactoRoutingModule],
+  imports: [
+    CommonModule,
+    ContactoRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyB4Df19pGwD_cBXN0h-Yb0RQehg1cd1vZc',
+    }),
+    ReactiveFormsModule,
+  ],
 })
 export class ContactoModule {}
